359af0816b4ae45f5f33f596eaedf6de103fed84
Baejunsoo <junsoo9023@gmail.com>
2025-03-14 17:06:12 +0900

    변경

---
+++ src/main/java/Code02_1.java
@@ -0,0 +1,14 @@
+public class Code02_1 {
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        int c = a + b;
+                         /*  printf(%d + %d = %d/n(줄바꿈), a, b, c);
+                             println(a + "+" + b + "=" + c);
+                         */
+        System.out.println(a + "+" + b + "=" + c);
+
+    }
+}
