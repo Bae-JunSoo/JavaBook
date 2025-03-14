aabd1ef65547baca0bf2780963b0d76317add99a
Baejunsoo <junsoo9023@gmail.com>
2025-03-14 17:25:21 +0900

    변경

---
+++ src/main/java/Code02_2.java
@@ -0,0 +1,21 @@
+public class Code02_2 {
+
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 100;
+        b = 50;
+
+        c = a + b;
+        System.out.println(a + "+" + b + "=" + c);
+
+        c = a - b;
+        System.out.println(a + "-" + b + "=" + c);
+
+        c = a * b;
+        System.out.println(a + "*" + b + "=" + c);
+
+        c = a / b;
+        System.out.println(a + "/" + b + "=" + c);
+    }
+}
