1c370f26f6652a3a654c279161e06791e4ae12a3
Baejunsoo <junsoo9023@gmail.com>
2025-03-14 18:04:05 +0900

    변경

---
+++ src/main/java/Code02_3.java
@@ -0,0 +1,14 @@
+public class Code02_3 {
+    public static void main(String[] args) {
+
+        /* String(문자열) str1(변수값)*/
+        String str1 = "난생처음";
+        String str2 = "자바";
+
+        System.out.print(str1);
+        System.out.println(str2);  /* ln = 줄바꿈 */
+
+        String  str3 = str1 + str2;
+        System.out.println(str3);
+    }
+}
