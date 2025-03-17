10125fc83af91724cf3931c94aad14b838f8b7c7
Baejunsoo <junsoo9023@gmail.com>
2025-03-17 18:31:27 +0900

    변경

---
+++ src/main/java/Code02_5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code02_5 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a;
+
+        a = s.nextInt();
+        System.out.println("사용자가 입력한 값 ==> " + a);
+
+        s.close();
+    }
+}
