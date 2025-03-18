9080fbddb3d643851b78f66b1844ca5c23833825
Baejunsoo <junsoo9023@gmail.com>
2025-03-18 16:02:31 +0900

    변경

---
+++ src/main/java/Code02_7.java
@@ -0,0 +1,17 @@
+public class Code02_7 {
+    public static void main(String[] args) {
+
+        int a;
+        double b;
+        String c;
+
+        c = "1234";
+        a = Integer.parseInt(c);
+        System.out.println("문자열 ==> 정수 : " + a);
+
+        c = "3.14";
+        b = Double.parseDouble(c);
+        System.out.println("문자열 ==> 실수 :" + b);
+
+    }
+}
