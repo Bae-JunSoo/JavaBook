0a68053e05b4558266515d2c613dc5e4fda09491
Baejunsoo <junsoo9023@gmail.com>
2025-03-20 17:30:09 +0900

    변경

---
+++ src/main/java/Code03_2.java
@@ -0,0 +1,22 @@
+public class Code03_2 {
+    public static void main(String[] args){
+
+
+        int a, b, c;
+        a = 5;
+        b = 3;
+
+        c = a + b;
+        System.out.println(c);
+        c = a - b;
+        System.out.println(c);
+        c = a * b;
+        System.out.println(c);
+        c = a / b;
+        System.out.println(c);
+        c = a % b;
+        System.out.println(c);
+
+
+    }
+}
