4b66065ff154f526643cf72c31d1c8784ec18a8b
Baejunsoo <junsoo9023@gmail.com>
2025-03-20 17:25:26 +0900

    변경

---
+++ src/main/java/Code03_1.java
@@ -0,0 +1,11 @@
+public class Code03_1 {
+    public static void main(String[] args) {
+
+        int a,b,c;
+        a = 5;
+        b = 3;
+        c = a + b;
+
+        System.out.println(c);
+    }
+}
