825b3832241135ce1ccc13dddf9b5a742ff8df81
Baejunsoo <junsoo9023@gmail.com>
2025-03-21 08:44:52 +0900

    변경

---
+++ src/main/java/Code03_4.java
@@ -0,0 +1,16 @@
+public class Code03_4 {
+    public static void main(String[] args) {
+
+        /* Math 함수 : 제곱 연산 Math.pow(밑수, 지수); */
+
+        double a, b;
+
+        /* Math 함수의 결과값은 실수로 표기 되기에 변수값을 double로 지정해야함 */
+
+        a = Math.pow(3, 2);
+        System.out.println(a);
+
+        b = Math.pow(4, 3);
+        System.out.println(b);
+    }
+}
