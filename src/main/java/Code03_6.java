2dbe46fe866c9f9dd1e195979fef49890dd54baf
Baejunsoo <junsoo9023@gmail.com>
2025-03-23 10:53:42 +0900

    변경

---
+++ src/main/java/Code03_6.java
@@ -0,0 +1,21 @@
+public class Code03_6 {
+    public static void main(String[] args) {
+
+        int num = 20;
+
+        num++ ;
+        System.out.print(num + " ");   // ++ = + 1
+        num-- ;
+        System.out.print(num + " ");   // -- = - 1
+        num += 3;
+        System.out.print(num + " ");
+        num -= 3;
+        System.out.print(num + " ");
+        num *= 3;
+        System.out.print(num + " ");
+        num /= 3;
+        System.out.print(num + " ");
+        num %= 3;
+        System.out.print(num + " ");
+    }
+}
