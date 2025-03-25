d24962ccd79cbed60745b1db32ec3cc394220de2
Baejunsoo <junsoo9023@gmail.com>
2025-03-25 18:19:14 +0900

    변경

---
+++ src/main/java/Code03_8.java
@@ -0,0 +1,10 @@
+public class Code03_8 {
+    public static void main(String[] args) {
+
+        int a = 99;
+
+        System.out.println((a > 100) && (a < 200) );    // && = 그리고
+        System.out.println((a == 99) || (a == 100) );   // || = 또는
+        System.out.println(!(a == 100) );               // ! = 부정
+    }
+}
