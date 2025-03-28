7d2adb16e399715e4f5498e4dfc106480ce2c497
Baejunsoo <junsoo9023@gmail.com>
2025-03-28 13:45:22 +0900

    변경

---
+++ src/main/java/Code_class4.java
@@ -0,0 +1,13 @@
+public class Code_class4 {
+    public static void main(String[] args) {
+
+        int a = 20;
+        a++ ; System.out.println(a+" ");
+        a--; System.out.println(a+" ");
+        a += 3; System.out.println(a+" ");
+        a -= 3; System.out.println(a+" ");
+        a *= 3; System.out.println(a+" ");
+        a /= 3; System.out.println(a+" ");
+        a %= 3; System.out.println(a+" ");
+    }
+}
