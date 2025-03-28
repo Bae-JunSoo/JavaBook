55ed67afab3070ae28758ca06e87da591ba4ada1
Baejunsoo <junsoo9023@gmail.com>
2025-03-28 14:08:54 +0900

    변경

---
+++ src/main/java/Code_class4_2.java
@@ -0,0 +1,10 @@
+public class Code_class4_2 {
+    public static void main(String[] args) {
+
+        int a = 99;
+
+        System.out.println((a > 100) && (a < 200));
+        System.out.println((a == 99) ||  (a == 100));
+        System.out.println( !(a == 100));
+    }
+}
