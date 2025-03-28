a3e60e3f1a025acd0cb67b40d4599954643de139
Baejunsoo <junsoo9023@gmail.com>
2025-03-28 14:56:54 +0900

    변경

---
+++ src/main/java/Code_class4_4.java
@@ -0,0 +1,10 @@
+public class Code_class4_4 {
+    public static void main(String[] args) {
+
+        String a = "작은따옴표는 \' 모양입니다.";   //    역슬래시(\)뒤에 따옴표를 쓰면 출력 가능 \" / \'
+        String b = "큰따옴표는 \" 모양입니다.";
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
