666811d71a93bad817de3600bf75ee668a33bdb3
Baejunsoo <junsoo9023@gmail.com>
2025-03-28 14:59:29 +0900

    변경

---
+++ src/main/java/Code_class4_5.java
@@ -0,0 +1,11 @@
+public class Code_class4_5 {
+    public static void main(String[] args) {
+
+        String a = "난생처음 \n" +
+                   "자바를 \n" +
+                   "열공 중입니다.";
+
+        System.out.println(a);
+
+    }
+}
