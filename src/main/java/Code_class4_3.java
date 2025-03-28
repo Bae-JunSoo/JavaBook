cfb15970a7c3bedb47fc2c2329023ccb9e5e7719
Baejunsoo <junsoo9023@gmail.com>
2025-03-28 14:24:26 +0900

    변경

---
+++ src/main/java/Code_class4_3.java
@@ -0,0 +1,13 @@
+public class Code_class4_3 {
+    public static void main(String[] args) {
+
+        boolean a, b;       // boolaen = 이진수 변수
+                            // 0 = false
+                            // 1 = true
+        a = true;
+        System.out.println(a);
+
+        b = (100 < 10);
+        System.out.println(b);
+    }
+}
