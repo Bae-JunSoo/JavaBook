b3464a56dc84b79dffc901540e363fa72b60a308
Baejunsoo <junsoo9023@gmail.com>
2025-03-28 15:10:04 +0900

    변경

---
+++ src/main/java/Code_class4_6.java
@@ -0,0 +1,12 @@
+public class Code_class4_6 {
+    public static void main(String[] args) {
+
+        String str = "난생처음 자바";
+        int len;
+
+        len = str.length();
+
+        System.out.println("문자열 : " + str);
+        System.out.println("문자열 길이 : " + len);
+    }
+}
