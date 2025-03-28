42205c811d7b55a0fb80c686c82261186401b4d4
Baejunsoo <junsoo9023@gmail.com>
2025-03-28 15:24:44 +0900

    변경

---
+++ src/main/java/Code_class4_8.java
@@ -0,0 +1,13 @@
+public class Code_class4_8 {
+    public static void main(String[] args) {
+
+        String a = "First Java를 공부 중입니다.";
+        String b, c;
+
+        b = a.toUpperCase();
+        System.out.println(b);
+        c = a.toLowerCase();
+        System.out.println(c);
+
+    }
+}
