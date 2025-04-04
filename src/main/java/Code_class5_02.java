45332668335f4b6036e5c0d81002569ab13c29ca
Baejunsoo <junsoo9023@gmail.com>
2025-04-04 14:20:17 +0900

    변경

---
+++ src/main/java/Code_class5_02.java
@@ -0,0 +1,10 @@
+public class Code_class5_02 {
+    public static void main(String[] args) {
+
+        String a = "난생처음 자바를 처음 학습 중입니다. 자바는 처음이지만 재미있네요.";
+
+        System.out.println(a.indexOf("처음"));
+        System.out.println(a.indexOf("처음"));
+        System.out.println(a.indexOf("처음" , 4));
+    }
+}
