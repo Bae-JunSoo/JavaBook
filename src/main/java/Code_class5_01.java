3214f2b04d7f6635c2c0da5a9914552a432b0f2b
Baejunsoo <junsoo9023@gmail.com>
2025-04-04 14:19:57 +0900

    변경

---
+++ src/main/java/Code_class5_01.java
@@ -0,0 +1,10 @@
+public class Code_class5_01 {
+    public static void main(String[] args) {
+
+        String a = "난생처음 자바";
+
+        System.out.println(a.indexOf("난생"));
+        System.out.println(a.indexOf("처"));
+
+    }
+}
