0d29376a4760b9a43f43e5c361e83f57f63bcf0c
Baejunsoo <junsoo9023@gmail.com>
2025-04-04 15:37:38 +0900

    변경

---
+++ src/main/java/Code_class6_02.java
@@ -0,0 +1,12 @@
+public class Code_class6_02 {
+    public static void main(String[] args) {
+
+        for (int i = 10;  i < 21; i++){
+            System.out.print(i + " ");
+        }
+
+        for (int i = 2; i < 21; i+=2){
+            System.out.print(i + " ");
+        }
+    }
+}
