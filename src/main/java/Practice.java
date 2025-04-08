c12af45d55e532831546df7b7ba57d37415f6d4a
Baejunsoo <junsoo9023@gmail.com>
2025-04-08 19:07:30 +0900

    변경

---
+++ src/main/java/Practice.java
@@ -0,0 +1,12 @@
+public class Practice {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + "번째 반복");
+        }
+
+        for (int i = 5; i <= 1; i--) {
+            System.out.println(i + "번 말할거야");
+        }
+    }
+}
