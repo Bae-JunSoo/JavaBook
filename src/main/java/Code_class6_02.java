8bbef62378fb24a9847034026a8e1cf50962a2e7
Baejunsoo <junsoo9023@gmail.com>
2025-04-10 18:12:27 +0900

    변경

--- src/main/java/Code_class6_02.java
+++ src/main/java/Code_class6_02.java
@@ -1,12 +1,12 @@
 public class Code_class6_02 {
     public static void main(String[] args) {
+        //구구단
 
-        for (int i = 10;  i < 21; i++){
-            System.out.print(i + " ");
-        }
-
-        for (int i = 2; i < 21; i+=2){
-            System.out.print(i + " ");
-        }
+       for (int i = 2; i <= 9; i++) {
+           for (int j = 1; j <= 9; j++) {
+               System.out.println(j + " * " + i + " = " + i*j);
+           }
+           System.out.println("---------------------------");
+       }
     }
 }
