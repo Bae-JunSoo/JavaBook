917d13d4403fc702eb81e70313379d483711466b
Baejunsoo <junsoo9023@gmail.com>
2025-06-16 18:19:55 +0900

    변경

---
+++ src/main/java/Code_class10_05.java
@@ -0,0 +1,12 @@
+public class Code_class10_05 {
+    static int para3_method(int a, int b, int c) {
+        int result = 0;
+        result = a +  b + c;
+        return result;
+    }
+    public static void main(String[] args) {
+        int hap;
+        hap = para3_method(10, 20, 30);
+        System.out.println("매개변수 3개 메서드 호출 결과 ==> "  + hap);
+    }
+}
